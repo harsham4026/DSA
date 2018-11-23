45cc2176397e9ebb7ff30ae69f76b8bd7d5e4576
Harsha Mandai <Harsha_Mandadi@intuit.com>
2018-11-23 18:18:44 +0530

    changes for removing dynamic allocation

--- grab_spark_streaming/src/main/StreamingJob.sh
+++ grab_spark_streaming/src/main/StreamingJob.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 spark-submit --class harsha.grab_spark_streaming.SparkStreaming --master yarn --deploy-mode cluster --conf spark.dynamicAllocation.enabled=true grab_spark_streaming-1.0-SNAPSHOT.jar /home/hadoop/grab_data_processing.properties
+
+#spark-submit --class harsha.grab_spark_streaming.SparkStreaming --master yarn --deploy-mode client --num-executors 2 --executor-cores 2 --executor-memory 5g --driver-memory 2g --driver-cores 1 grab_spark_streaming-1.0-SNAPSHOT.jar /home/hadoop/grab_data_processing.properties
