f96ed496296b3b1a47daf3feee9bbda468fdc525
Harsha Mandadi <42734799+harsham4026@users.noreply.github.com>
2019-03-03 16:18:38 +0530

    intersection of array without and with duplicates

---
+++ dsa/IntersectionOfArrayWithDuplicates.py
@@ -0,0 +1,38 @@
+def intersection_of_arrays(arr1, arr2, m, n):
+    result_array = []
+    i = j = 0
+    while i < m and j < n:
+        if arr1[i] < arr2[j]:
+            i += 1
+        elif arr2[j] < arr1[i]:
+            j += 1
+        else:
+            result_array.append(arr2[j])
+            i += 1
+            j += 1
+    return result_array
+
+
+def dict_approach(arr1, arr2, m, n):
+    common_list = []
+    dict = {}
+
+    for i in range(m):
+        if arr1[i] in dict:
+            dict[arr1[i]] = dict[arr1[i]] + 1
+        else:
+            dict[arr1[i]] = 1
+
+    for i in range(n):
+        if arr2[i] in dict and dict[arr2[i]] > 0:
+            common_list.append(arr2[i])
+            dict[arr2[i]] = dict[arr2[i]] - 1
+
+    return common_list
+
+
+if __name__ == '__main__':
+    arr1 = [1, 2, 1, 1, 3]
+    arr2 = [1, 1, 1, 2]
+    print(intersection_of_arrays(arr1, arr2, len(arr1), len(arr2)))
+    print(dict_approach(arr1, arr2, len(arr1), len(arr2)))
