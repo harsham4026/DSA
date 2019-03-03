aa975ce7ba7da923193a987b5d21a2614b99c5e6
Harsha Mandadi <42734799+harsham4026@users.noreply.github.com>
2019-03-03 16:50:11 +0530

    segregate zeros to left and right of array both code is included

---
+++ dsa/SegreagteZerosAndOnes.py
@@ -0,0 +1,44 @@
+# take the counter and iterate over the array and see if the element not equals to 0 then at the index of count put the
+# value of arr[i]. In the next for loop the number of zeros we have in array is calculated as len(arr) - count.
+# In the next for loop
+
+def segregate_zeros_and_ones(arr):
+    count = 0
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+
+    for i in range(count, len(arr)):
+        arr[i] = 0
+
+    return arr
+
+def segragate_zeros_to_left(arr) :
+    count = 0
+
+    length = len(arr)
+    new_array = [None] * length
+
+    for i in range(length) :
+        if arr[i] == 0 :
+            count += 1
+
+    for i in range(0, count) :
+        new_array[i] = 0
+
+    for i in range(length) :
+        if arr[i] != 0 :
+            new_array[count] = arr[i]
+            count += 1
+
+    return new_array
+
+
+if __name__ == '__main__':
+    arr = [0, 1, 1, 0, 1, 0, 0, 1, 1]
+    print(segregate_zeros_and_ones(arr))
+
+    arr2 = [1, 2, 0, 4, 5, 0, 6, 0, 0, 0]
+    print(segragate_zeros_to_left(arr2))
