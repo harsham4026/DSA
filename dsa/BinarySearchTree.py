3f443ae1d0d2ffd6d20e86f9446ba1f41e714ea9
Harsha Mandai <Harsha_Mandadi@intuit.com>
2019-03-09 19:33:37 +0530

    changes

--- dsa/BinarySearchTree.py
+++ dsa/BinarySearchTree.py
@@ -15,6 +15,22 @@ def height_of_tree(root):
     return max(lheight, rheight) + 1
 
 
+def insert(root, node):
+    if root is None:
+        root = node
+    else:
+        if node.data < root.data:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right, node)
+
+
 def get_width_of_a_level(root, level):
     if root is None:
         return 0
@@ -76,6 +92,61 @@ def printPreorder(root):
         printPreorder(root.right)
 
 
+def minValueNode(node):
+    current = node
+
+    # loop down to find the leftmost leaf
+    while current.left is not None:
+        current = current.left
+
+    return current
+
+
+# Given a binary search tree and a key, this function
+# delete the key and returns the new root
+def deleteNode(root, key):
+    # Base Case
+    if root is None:
+        return root
+
+        # If the key to be deleted is smaller than the root's
+    # key then it lies in  left subtree
+    if key < root.data:
+        root.left = deleteNode(root.left, key)
+
+        # If the kye to be delete is greater than the root's key
+    # then it lies in right subtree
+    elif (key > root.data):
+        root.right = deleteNode(root.right, key)
+
+        # If key is same as root's key, then this is the node
+    # to be deleted
+    else:
+
+        # Node with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+            # Node with two children: Get the inorder successor
+        # (smallest in the right subtree)
+        temp = minValueNode(root.right)
+
+        # Copy the inorder successor's content to this node
+        root.data = temp.data
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, temp.data)
+
+    return root
+
+
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
@@ -88,3 +159,8 @@ if __name__ == '__main__':
     print("max width of the given binary tree is : {}".format(str(max_width_of_tree(root))))
     print(root.data)
     printInorder(root)
+
+    # root = deleteNode(root, 50)
+    # print(
+    # "Inorder traversal of the modified tree")
+    # printInorder(root)
