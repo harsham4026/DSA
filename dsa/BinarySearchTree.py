e4c5506d82dc43dcdeb04c37487b1e0d827f6ead
Harsha Mandadi <42734799+harsham4026@users.noreply.github.com>
2019-03-09 12:22:17 +0530

    Update BinarySearchTree.py

--- dsa/BinarySearchTree.py
+++ dsa/BinarySearchTree.py
@@ -5,186 +5,42 @@ class Node:
         self.right = None
 
 
-def insert(root, node):
-    if root is None:
-        root = node
-
-    else:
-        if node.data < root.data:
-            if root.left is None:
-                root.left = node
-            else:
-                insert(root.left, node)
-        else:
-            if root.right is None:
-                root.right = node
-
-            else:
-                insert(root.right, node)
-
-
-def print_the_tree_inorder(root):
-    if root is not None:
-        print_the_tree_inorder(root.left)
-        print(root.data)
-        print_the_tree_inorder(root.right)
-
-
-def print_the_tree_preorder(root):
-    if root is not None:
-        print(root.data)
-        print_the_tree_preorder(root.left)
-        print_the_tree_preorder(root.right)
-
-
-def print_the_tree_postorder(root):
-    if root is not None:
-        print_the_tree_postorder(root.left)
-        print_the_tree_postorder(root.right)
-        print(root.data)
-
-
-def print_at_given_level(root, level):
-    if root is None:
-        return
-    elif level == 1:
-        print(root.data)
-    elif level > 1:
-        print_at_given_level(root.left, level - 1)
-        print_at_given_level(root.right, level - 1)
-
-
-def breadth_first_search_or_level_order_tree_traversal(root):
-    height = max_depth_of_a_bst(root)
-    for i in range(1, height + 1):
-        print_at_given_level(root, i)
-
-
-def max_depth_of_a_bst(root):
-    if root is None:
-        return 0
-    else:
-        ldepth = max_depth_of_a_bst(root.left)
-        rdepth = max_depth_of_a_bst(root.right)
-
-        if ldepth > rdepth:
-            return ldepth + 1
-        else:
-            return rdepth + 1
-
-
-def max_value_in_the_tree(root):
+def height_of_tree(root):
     if root is None:
         return 0
 
-    res = root.data
+    lheight = height_of_tree(root.left)
+    rheight = height_of_tree(root.right)
 
-    lres = max_value_in_the_tree(root.left)
-    rres = max_value_in_the_tree(root.right)
+    return max(lheight, rheight) + 1
 
-    # if lres > res:
-    #     res = lres
-    # if rres > res:
-    #     res = rres
-    # return res
-    return max(lres, rres, res)
-
-
-def find_max(root):
-    current = root
-
-    while current.right is not None:
-        current = current.right
-
-    return current.data
-
-
-def find_min(root):
-    current = root
-
-    while current.left is not None:
-        current = current.left
-
-    return current.data
-
-
-def delete_the_node(root, key):
-    if root is None:
-        return root
-
-    if key > root.data:
-        root.right = delete_the_node(root.right, key)
-
-    elif key < root.data:
-        root.left = delete_the_node(root.left, key)
-
-    else:
-        if root.right is None:
-            temp = root.left
-            root = None
-            return temp
-        elif root.left is None:
-            temp = root.right
-            root = None
-            return temp
-
-            # Node with two children: Get the inorder successor
-            # (smallest in the right subtree)
-
-        temp = find_min(root.right)
-
-        # Copy the inorder successor's content to this node
-        root.data = temp.data
-
-        # Delete the inorder successor
-        root.right = delete_the_node(root.right, temp.data)
-
-        return root
-
-
-def get_width_at_given_level(root, level):
-    if root is None:
+def get_width_of_a_level(root, level) :
+    if root is None :
         return 0
     elif level == 1:
         return 1
-    elif level > 1:
-        return get_width_at_given_level(root.left, level - 1) + get_width_at_given_level(root.right, level - 1)
+    else:
+        return get_width_of_a_level(root.left, level - 1) + get_width_of_a_level(root.right, level - 1)
 
 
-def get_width_of_binary_tree(root):
-    height = max_depth_of_a_bst(root)
+def max_width_of_tree(root):
+    height = height_of_tree(root)
     max_width = 0
 
     for i in range(1, height + 1):
-        width = get_width_at_given_level(root, i)
-        if width > max_width:
+        width = get_width_of_a_level(root, i)
+        if width > max_width :
             max_width = width
-
     return max_width
 
 
-r = Node(50)
-insert(r, Node(30))
-insert(r, Node(20))
-insert(r, Node(40))
-insert(r, Node(70))
-insert(r, Node(60))
-insert(r, Node(80))
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(7)
+root.left.right = Node(6)
+root.right.right = Node(4)
+root.right.left = Node(5)
 
 if __name__ == '__main__':
-    print_the_tree_inorder(r)
-    print_the_tree_preorder(r)
-    print_the_tree_postorder(r)
-
-    print("height of the tree : " + str(max_depth_of_a_bst(r)))
-    print("diameter of the tree : " + str(max_depth_of_a_bst(r)))
-
-    print(max_value_in_the_tree(r))
-
-    print(find_max(r))
-    print(find_min(r))
-
-    print("BFS Tree")
-    breadth_first_search_or_level_order_tree_traversal(r)
-
-    print("width of binary tree : {}".format(str(get_width_of_binary_tree(r))))
+    print("max width of the given binary tree is : {}".format(str(max_width_of_tree(root))))
