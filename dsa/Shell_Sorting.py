2c50a9f0cca11e5fb94844803b91d8a0e5a7face
Harsha Mandai <Harsha_Mandadi@intuit.com>
2019-03-15 08:09:50 +0530

    shell sorting code added

--- dsa/Shell_Sorting.py
+++ dsa/Shell_Sorting.py
@@ -1,26 +1,28 @@
 def shellSort(alist):
-	sublistcount = len(alist)//2
-	while sublistcount > 0:
+    sublistcount = len(alist) // 2
+    while sublistcount > 0:
 
-		for startposition in range(sublistcount):
-			gapInsertionSort(alist,startposition,sublistcount)
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
 
-		print("After increments of size",sublistcount,"The list is",alist)
+        print("After increments of size", sublistcount, "The list is", alist)
 
-		sublistcount = sublistcount // 2
+        sublistcount = sublistcount // 2
 
-def gapInsertionSort(alist,start,gap):
-	for i in range(start+gap,len(alist),gap):
 
-		currentvalue = alist[i]
-		position = i
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
 
-		while position>=gap and alist[position-gap]>currentvalue:
-			alist[position]=alist[position-gap]
-			position = position-gap
+        currentvalue = alist[i]
+        position = i
 
-		alist[position]=currentvalue
+        while position >= gap and alist[position - gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position = position - gap
 
-alist = [54,26,93,17,77,31,44,55,20]
+        alist[position] = currentvalue
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 shellSort(alist)
 print(alist)
