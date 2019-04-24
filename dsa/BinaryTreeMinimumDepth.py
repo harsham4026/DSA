4c43c416d75caf616445552f22f2305a3e262322
Harsha Mandai <Harsha_Mandadi@intuit.com>
2019-04-24 19:24:28 +0530

    binary tree paths printing from root to leaves

--- dsa/BinaryTreeMinimumDepth.py
+++ dsa/BinaryTreeMinimumDepth.py
@@ -13,6 +13,19 @@ def get_minimum_depth(root):
     return 1 + min(left_depth, right_depth)
 
 
+def print_paths_from_root_to_leaves(root, stack):
+    if root is None:
+        return
+
+    stack.append(root.data)
+
+    if root.left is None and root.right is None:
+        print('->'.join([str(i) for i in stack]))
+
+    print_paths_from_root_to_leaves(root.left, stack)
+    print_paths_from_root_to_leaves(root.right, stack)
+    stack.pop()
+
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
@@ -29,3 +42,4 @@ root.left.right = Node(6)
 
 if __name__ == '__main__':
     print(get_minimum_depth(root))
+    print_paths_from_root_to_leaves(root, [])
